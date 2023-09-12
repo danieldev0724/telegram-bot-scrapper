5cae42dfc3a76c9acbfc66ee9544b474a62f7adf
danieldev0724 <danielchendev0724@gmail.com>
2023-09-13 00:40:38 +0800

    ADD: Startup event

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 import uvicorn
 from starlette.responses import RedirectResponse
 from core.manage import settings
+from core.initializers import *
 from api.v1.controllers.telegram_bot_webhook import webhooks_router
+
+# Server APP
+def run_server(): uvicorn.run('core:app',host=settings.HOST,port=8000,reload=True)
+
 
 # Start APP 
 app = FastAPI(
@@ -17,8 +22,16 @@ app = FastAPI(
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/api-docs")
-    
+
+# Set global events
+async def startup_event():
+    print('-- START API BOT TELEGRAM --')
+
+# execute global events
+@app.on_event("startup")
+async def on_startup():
+    await startup_event()
+
+
 # Routers
 app.include_router(webhooks_router, prefix="/api/v1")
-
-def run_server(): uvicorn.run('core:app',host=settings.HOST,port=8000,reload=True)
