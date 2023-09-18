43160aca77077358ed0b4cd5d436ad5b7ebb1218
danieldev0724 <danielchendev0724@gmail.com>
2023-09-19 02:13:55 +0900

    UPDATE: Refactor and optimization improvements

---
+++ app/core/tasks.py
@@ -0,0 +1,12 @@
+from . init_bot import bot
+import asyncio
+from core.logger import logger
+
+"""ASYNC"""
+async def photo(): pass
+    #await bot.send_photo()
+
+def exec_async():
+    asyncio.run(photo())
+
+"""SYNC"""
